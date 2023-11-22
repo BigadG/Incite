a85b0742c4a6e187ecd368953b0b021c849ba3d9
Seif Gaber <bigadgaber@icloud.com>
2023-11-22 16:40:51 -0600

    Create app.test.jsx to see if testing env works
    (it doesn't right now)

--- client/src/app.test.jsx
+++ client/src/app.test.jsx
@@ -1,49 +1,24 @@
-//This is just an example of how I will have to run tests
-//For every component, I would create one of these component.test.jsx files
-
-import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App Component', () => {
-  test('renders Vite + React header', () => {
+  test('renders without crashing', () => {
     render(<App />);
-    const headerElement = screen.getByText(/Vite \+ React/i);
-    expect(headerElement).toBeInTheDocument();
+    expect(screen.getByText('Vite + React')).toBeInTheDocument();
   });
 
-  test('renders images', () => {
-    render(<App />);
-    const viteLogo = screen.getByAltText('Vite logo');
-    const reactLogo = screen.getByAltText('React logo');
-    expect(viteLogo).toBeInTheDocument();
-    expect(reactLogo).toBeInTheDocument();
-  });
-
-  test('initial count is 0', () => {
-    render(<App />);
-    const countElement = screen.getByText(/count is 0/i);
-    expect(countElement).toBeInTheDocument();
-  });
-
-  test('increments count on button click', () => {
+  test('renders increment button', () => {
     render(<App />);
     const buttonElement = screen.getByText(/count is/i);
-    fireEvent.click(buttonElement);
-    const countElement = screen.getByText(/count is 1/i);
-    expect(countElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
   });
 
-  test('contains link to Vite and React documentation', () => {
+  test('renders links to Vite and React', () => {
     render(<App />);
     const viteLink = screen.getByRole('link', { name: /Vite logo/i });
     const reactLink = screen.getByRole('link', { name: /React logo/i });
-    expect(viteLink).toHaveAttribute('href', 'https://vitejs.dev');
-    expect(reactLink).toHaveAttribute('href', 'https://react.dev');
-  });
-
-  test('renders edit instructions', () => {
-    render(<App />);
-    const editInstructions = screen.getByText(/Edit src\/App\.jsx and save to test HMR/i);
-    expect(editInstructions).toBeInTheDocument();
+    expect(viteLink).toBeInTheDocument();
+    expect(reactLink).toBeInTheDocument();
   });
 });
