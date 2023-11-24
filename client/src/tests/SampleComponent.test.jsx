b9e14e53d2ca111e1fe2aa518bf94c1b45de42cc
Seif Gaber <bigadgaber@icloud.com>
2023-11-24 11:55:27 -0600

    Create test and update package.json

---
+++ client/src/tests/SampleComponent.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SampleComponent from '../components/SampleComponent';
+
+test('renders SampleComponent', () => {
+  render(<SampleComponent />);
+  expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+});
