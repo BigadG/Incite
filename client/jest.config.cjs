12188cf01dd712b00b83775b6a3f1d8456018246
Seif Gaber <bigadgaber@icloud.com>
2023-11-26 11:13:46 -0600

    Add setupFilesAfterEnv

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,17 +1,19 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-    testEnvironment: 'jsdom',
-    testMatch: [
-      '**/tests/**/*.[jt]s?(x)',
-      '**/?(*.)+(spec|test).[tj]s?(x)'
-    ],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/__mocks__/fileMock.js'
-    },
-  };
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/tests/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
   
