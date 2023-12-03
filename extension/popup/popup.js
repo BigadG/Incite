de14127b1e8b393bdf47cd3cb249b153b3101c1a
Seif Gaber <bigadgaber@icloud.com>
2023-12-03 17:34:28 -0600

    Create popup.js with:
    Variables to be connected to popup.html.
    'addSelections' sends URL to server to add to selection list.
    'showSelections' gets&logs selection list.
    'clearSelections' sends request to clear selections list

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,25 +1,98 @@
-// Function to send a POST request to the server to add a selection
-async function addSelection(url, title) {
-    const response = await fetch('http://localhost:3000/addSelection', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ url, title }),
+// popup.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Define the server endpoint URL
+    const serverUrl = 'http://localhost:3000';
+  
+    // Button references
+    const addButton = document.getElementById('addButton');
+    const showButton = document.getElementById('showButton');
+    const clearButton = document.getElementById('clearButton');
+  
+    // Function to send a POST request to the server to add a selection
+    async function addSelection(url, title) {
+      try {
+        const response = await fetch(`${serverUrl}/addSelection`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            // Add any necessary headers like authorization tokens here
+          },
+          body: JSON.stringify({ url, title }),
+        });
+  
+        if (response.ok) {
+          console.log('Selection added');
+        } else {
+          throw new Error('Failed to add selection');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    // Function to get the user's selections from the server
+    async function showSelections() {
+      try {
+        const response = await fetch(`${serverUrl}/selections`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Add any necessary headers like authorization tokens here
+          },
+        });
+  
+        if (response.ok) {
+          const selections = await response.json();
+          // Code to display the selections to the user goes here
+          console.log(selections);
+        } else {
+          throw new Error('Failed to retrieve selections');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    // Function to clear the user's selections from the server
+    async function clearSelections() {
+      try {
+        const response = await fetch(`${serverUrl}/clearSelections`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            // Add any necessary headers like authorization tokens here
+          },
+          // Send necessary data like userId if required
+          body: JSON.stringify({ /* userId or other required fields */ }),
+        });
+  
+        if (response.ok) {
+          console.log('Selections cleared');
+        } else {
+          throw new Error('Failed to clear selections');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    // Event listeners for buttons
+    addButton.addEventListener('click', function() {
+      // Logic to retrieve the current tab's URL and title
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const currentTab = tabs[0];
+        addSelection(currentTab.url, currentTab.title);
+      });
     });
   
-    if (response.ok) {
-      console.log('Selection added');
-    } else {
-      console.error('Failed to add selection');
-    }
-  }
+    showButton.addEventListener('click', function() {
+      showSelections();
+    });
   
-  // Add event listener to your 'Add to selections' button
-  document.getElementById('addButton').addEventListener('click', () => {
-    // You would get the current tab's URL and title here
-    const url = 'the current tab url';
-    const title = 'the current tab title';
-    addSelection(url, title);
+    clearButton.addEventListener('click', function() {
+      clearSelections();
+    });
   });
+  
   
--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,16 @@
 const { connect } = require('./database');
 const express = require('express');
+const { ObjectId } = require('mongodb'); // Make sure to import ObjectId
 const router = express.Router();
 
 // POST route to add selection
 router.post('/addSelection', async (req, res) => {
   try {
     const db = await connect();
+    // Extract userId from request, assuming authentication is handled upstream
     const { userId, title, url } = req.body;
     // Assuming you have a unique identifier for each user, e.g., userId.
+    // ObjectId is used to create a unique identifier for the selection
     const result = await db.collection('Users').updateOne(
       { userId },
       { $push: { selections: { title, url, pageId: new ObjectId(), timestamp: new Date() } } },
@@ -21,29 +24,35 @@ router.post('/addSelection', async (req, res) => {
 
 // GET route to retrieve selections
 router.get('/selections/:userId', async (req, res) => {
-    try {
-      const db = await connect();
-      const userId = req.params.userId; // Assuming you're passing userId as a URL parameter.
-      const user = await db.collection('Users').findOne({ userId });
-      res.status(200).json(user ? user.selections : []);
-    } catch (error) {
-      res.status(500).json({ message: 'Error retrieving selections', error });
-    }
-  });
+  try {
+    const db = await connect();
+    // Extract userId from request parameters
+    const userId = req.params.userId; // Assuming you're passing userId as a URL parameter.
+    const user = await db.collection('Users').findOne({ userId });
+    // If user is found, return their selections, otherwise return an empty array
+    res.status(200).json(user ? user.selections : []);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving selections', error });
+  }
+});
 
 // POST route to clear the selections
 router.post('/clearSelections', async (req, res) => {
-    try {
-      const db = await connect();
-      const { userId } = req.body; // Replace with your actual request body fields.
-      const result = await db.collection('Users').updateOne(
-        { userId },
-        { $set: { selections: [] } }
-      );
-      res.status(200).json({ message: 'Selections cleared', result });
-    } catch (error) {
-      res.status(500).json({ message: 'Error clearing selections', error });
-    }
-  });
+  try {
+    const db = await connect();
+    // Extract userId from request body, assuming authentication is handled upstream
+    const { userId } = req.body;
+    // Clear the selections array for the user
+    const result = await db.collection('Users').updateOne(
+      { userId },
+      { $set: { selections: [] } }
+    );
+    res.status(200).json({ message: 'Selections cleared', result });
+  } catch (error) {
+    res.status(500).json({ message: 'Error clearing selections', error });
+  }
+});
 
+// Export the router to be used in server.js
 module.exports = router;
+
