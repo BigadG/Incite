6a275cc55e13258cbc7ea39e73d1e19df700d276
Seif Gaber <bigadgaber@icloud.com>
2023-12-04 11:59:36 -0600

    Make test suite cover:
    Adding selection w proper auth
    Retrieving selection for user
    Clearing selection for user
    Failing to add selection without auth
    Handle errors when adding w missing data

--- server/tests/userSelections.test.js
+++ server/tests/userSelections.test.js
@@ -2,10 +2,26 @@
 const request = require('supertest');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const { MongoClient } = require('mongodb');
-const app = require('../server'); // Ensure this points to your Express app
+const jwt = require('jsonwebtoken');
+const app = require('../server'); // Make sure this points to your Express app
+
+jest.mock('../middleware/authMiddleware', () => ({
+  authMiddleware: (req, res, next) => {
+    // Mock user object
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  }
+}));
 
 let mongoServer;
 let db;
+let authToken;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -14,10 +30,14 @@ beforeAll(async () => {
   await client.connect();
   db = client.db('InciteTestDB');
 
-  // Replacing the connect method to use the in-memory server
+  // Replace the actual connect method with the connection to the in-memory database
   jest.mock('../database', () => ({
     connect: () => db
   }));
+
+  // Create a mock user and generate a test JWT
+  const testUser = { userId: 'testUser', email: 'test@example.com' };
+  authToken = jwt.sign(testUser, process.env.JWT_SECRET, { expiresIn: '1h' });
 });
 
 afterAll(async () => {
@@ -27,42 +47,72 @@ afterAll(async () => {
 });
 
 describe('User Selections', () => {
-  test('It should add a selection', async () => {
-    // Mock auth if needed or add a test-specific middleware
-    // ...
+  beforeEach(async () => {
+    await db.collection('Users').insertOne({
+      userId: 'testUser',
+      selections: []
+    });
+  });
 
+  afterEach(async () => {
+    await db.collection('Users').deleteMany({});
+  });
+
+  test('It should add a selection', async () => {
     const response = await request(app)
-      .post('/api/add-selection')
-      .send({ userId: 'testUserId', title: 'Test Title', url: 'http://test.com' })
+      .post('/api/addSelection')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({ userId: 'testUser', title: 'Test Title', url: 'http://test.com' })
       .expect(200);
 
     expect(response.body.message).toBe('Selection added');
-    // Additional assertions as needed
   });
 
   test('It should retrieve selections', async () => {
-    // Populate the database with a test entry
-    // ...
+    await db.collection('Users').updateOne(
+      { userId: 'testUser' },
+      { $push: { selections: { title: 'Test Title', url: 'http://test.com', pageId: new ObjectId(), timestamp: new Date() } } }
+    );
 
     const response = await request(app)
-      .get('/api/selections/testUserId')
+      .get('/api/selections/testUser')
+      .set('Authorization', `Bearer ${authToken}`)
       .expect(200);
 
-    expect(response.body).toEqual([
-      // Expected selections array
-    ]);
+    expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining({ title: 'Test Title', url: 'http://test.com' })]));
   });
 
   test('It should clear selections', async () => {
-    // Ensure there are selections to clear
-    // ...
+    await db.collection('Users').updateOne(
+      { userId: 'testUser' },
+      { $set: { selections: [{ title: 'Test Title', url: 'http://test.com', pageId: new ObjectId(), timestamp: new Date() }] } }
+    );
 
     const response = await request(app)
       .post('/api/clearSelections')
-      .send({ userId: 'testUserId' })
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({ userId: 'testUser' })
       .expect(200);
 
     expect(response.body.message).toBe('Selections cleared');
-    // Additional assertions as needed
+  });
+
+  test('It should fail to add a selection without authentication', async () => {
+    const response = await request(app)
+      .post('/api/addSelection')
+      .send({ userId: 'testUser', title: 'Test Title', url: 'http://test.com' })
+      .expect(401);
+
+    expect(response.body.message).toBe('Authentication failed');
+  });
+
+  test('It should handle errors when adding a selection with missing data', async () => {
+    const response = await request(app)
+      .post('/api/addSelection')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({ userId: 'testUser', title: 'Test Title' }) // Missing URL
+      .expect(400);
+
+    expect(response.body.message).toContain('Missing url');
   });
 });
