84ed63cdac9bb15236b0874f4425b17851144e10
Seif Gaber <bigadgaber@icloud.com>
2023-12-04 15:51:54 -0600

    Only run server if file is run directly

--- server/server.js
+++ server/server.js
@@ -1,26 +1,24 @@
-require('dotenv').config(); // Require dotenv at the top to use environment variables
+require('dotenv').config();
 const express = require('express');
-const routes = require('./routes'); // Import the router from routes.js
-const { authMiddleware } = require('./authMiddleware'); // Correctly destructure if it's a named export
+const routes = require('./routes');
+const { authMiddleware } = require('./authMiddleware');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-
-// Apply the auth middleware to all routes starting with '/api'
-// It's important to put this line before the routes so that it applies the middleware
 app.use('/api', authMiddleware);
-
-// Mount the routes from routes.js to the '/api' path
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('Incite Server is running!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+// Start the server only if running this file directly (i.e., not in test mode)
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
 
 module.exports = app;
