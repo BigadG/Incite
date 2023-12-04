d3e909a7bac04628999af0cb30c5b13d663af1b6
Seif Gaber <bigadgaber@icloud.com>
2023-12-04 10:17:38 -0600

    Setup cron job for mongoDB backups
    Implement MongoDB Atlas for monitering tools
    Install Axios

--- server/database.js
+++ server/database.js
@@ -1,16 +1,22 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
+
+const password = process.env.MONGO_ATLAS_PASSWORD; // Make sure to set this environment variable.
+const dbName = 'InciteDB'; // Your database name.
+const url = `mongodb+srv://bigadgaber:${password}@cluster0.bz2has9.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(url);
 
 async function connect() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-    const db = client.db('InciteDB');
+    console.log('Connected to MongoDB Atlas');
+    const db = client.db(dbName);
     return db;
   } catch (e) {
-    console.error(e);
+    console.error('Failed to connect to MongoDB Atlas', e);
   }
 }
 
 module.exports = { connect };
+
