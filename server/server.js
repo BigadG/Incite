ec885f92cf81b3f027c99b0554f01a3aae5e9654
Seif Gaber <bigadgaber@icloud.com>
2023-12-04 10:23:12 -0600

    Update server.js to use routes

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 // server/server.js
+require('dotenv').config(); // Require dotenv at the top to use environment variables
 const express = require('express');
-const authMiddleware = require('./authMiddleware'); // Import the middleware
+const routes = require('./routes'); // Import the router from routes.js
+const { authMiddleware } = require('./middleware/authMiddleware'); // Correctly destructure if it's a named export
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-// Use the auth middleware for routes that need protection
+// Apply the auth middleware to all routes starting with '/api'
+// It's important to put this line before the routes so that it applies the middleware
 app.use('/api', authMiddleware);
+
+// Mount the routes from routes.js to the '/api' path
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('Incite Server is running!');
-});
-
-// Secure this route with the auth middleware
-app.post('/api/add-selection', authMiddleware, (req, res) => {
-  // Your secure logic here
-  res.status(200).send('Selection added successfully');
 });
 
 app.listen(port, () => {
