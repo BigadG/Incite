1e8f1ab1119255f547e859b280fda30a3fbcfcf1
Seif Gaber <bigadgaber@icloud.com>
2023-12-10 12:59:55 -0600

    Render Popup.jsx instead of App.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Popup from './components/Popup';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Popup />
   </React.StrictMode>,
-)
+);
+
