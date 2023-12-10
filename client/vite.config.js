cf814b05a3f15efe5538247023ea54cfb421eafb
Seif Gaber <bigadgaber@icloud.com>
2023-12-10 14:57:28 -0600

    make vite.config point to correct paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,22 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        popup: resolve(__dirname, 'public', 'popup.html'),
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        main: resolve(__dirname, 'src', 'main.jsx'), // Entry point for application
+        popup: resolve(__dirname, 'public', 'popup.html') // Entry point for popup
       }
     }
   }
 });
+
 
