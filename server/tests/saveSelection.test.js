7e1a71ef90846705f8edb420c89a99c55828718c
Seif Gaber <bigadgaber@icloud.com>
2023-12-10 12:14:58 -0600

    Temporary flawed test to be changed later

--- server/tests/saveSelection.test.js
+++ server/tests/saveSelection.test.js
@@ -1,22 +1,39 @@
 const request = require('supertest');
 const app = require('../server');
-const { connect, closeDatabase } = require('../database');
-
-beforeAll(async () => await connect());
-afterAll(async () => await closeDatabase());
+const { MongoClient } = require('mongodb');
 
 describe('POST /api/addSelection', () => {
-  it('should add a selection to the user\'s list', async () => {
-    const uuid = 'test-uuid'; // Example UUID for testing
-    const url = 'http://example.com';
-    const title = 'Example Title';
-    
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+    });
+    db = await connection.db(global.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  test('It should respond with a 200 status and confirmation message', async () => {
+    const newSelection = {
+      url: 'http://example.com',
+      title: 'Example Website'
+    };
+
     const response = await request(app)
       .post('/api/addSelection')
       .set('Authorization', `Bearer ${uuid}`)
-      .send({ url, title });
-
+      .send(newSelection);
+    
     expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe('Selection added');
+    expect(response.body.message).toEqual('Selection added');
+    
+    // Verify the database was updated
+    const selections = db.collection('Users').findOne({ uuid });
+    expect(selections).toBeTruthy();
+    expect(selections.selections).toContainEqual(expect.objectContaining(newSelection));
   });
 });
