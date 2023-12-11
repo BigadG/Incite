ec1737096d1edf3d3c932d5d1679a8692dea3faf
Seif Gaber <bigadgaber@icloud.com>
2023-12-11 09:38:40 -0600

    Try StrictMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Popup from './components/Popup';
+import Popup from './components/Popup.jsx';
 import './styles/popup.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
+  document.getElementById('root')
 );
 
