98ed905c383337d5bf90fc2f94e9765d3dbb5340
Seif Gaber <bigadgaber@icloud.com>
2023-12-12 10:10:49 -0600

    Try simplifying test to pass pipeline

--- server/tests/userSelections.test.js
+++ server/tests/userSelections.test.js
@@ -3,23 +3,21 @@ const request = require('supertest');
 const { MongoClient, ObjectId } = require('mongodb');
 const app = require('../server'); // Ensure this points to your Express app
 
-jest.mock('../authMiddleware', () => {
-  return {
-    authMiddleware: (req, res, next) => {
-      try {
-        const uuid = req.headers.authorization.split(' ')[1];
-        if (uuid) {
-          req.userId = uuid; // Attach the UUID to the request object
-          next();
-        } else {
-          throw new Error('Authentication UUID is missing');
-        }
-      } catch (error) {
-        return res.status(401).json({ message: 'Authentication failed', error: error.message });
+jest.mock('../authMiddleware', () => ({
+  authMiddleware: (req, res, next) => {
+    try {
+      const uuid = req.headers.authorization.split(' ')[1];
+      if (uuid) {
+        req.userId = uuid; // Attach the UUID to the request object
+        next();
+      } else {
+        throw new Error('Authentication UUID is missing');
       }
+    } catch (error) {
+      return res.status(401).json({ message: 'Authentication failed', error: error.message });
     }
-  };
-});
+  }
+}));
 
 jest.mock('../database', () => {
   const { MongoClient } = require('mongodb'); // Import MongoClient inside the mock
@@ -48,7 +46,7 @@ jest.mock('../database', () => {
 
 let mongoServer;
 let db;
-let authToken;
+let authToken = 'mock-uuid-1234'; // Use a mock UUID
 
 beforeAll(async () => {
   const { MongoMemoryServer } = require('mongodb-memory-server');
@@ -57,9 +55,6 @@ beforeAll(async () => {
   db = new MongoClient(uri);
   await db.connect();
   await db.db("InciteTestDB").command({ ping: 1 });
-
-  // Use a mock UUID
-  authToken = 'mock-uuid-1234';
 });
 
 afterAll(async () => {
@@ -71,68 +66,60 @@ afterAll(async () => {
   }
 });
 
+beforeEach(async () => {
+  db = await require('../database').connect();
+  await db.collection('Users').deleteMany({});
+  await db.collection('Users').insertOne({
+    userId: authToken,
+    selections: []
+  });
+});
+
 describe('User Selections', () => {
-  let db;
-
-  beforeEach(async () => {
-    db = await require('../database').connect();
-    await db.collection('Users').insertOne({
-      userId: authToken, // Use UUID here
-      selections: []
-    });
-  });
-
-  afterEach(async () => {
-    await db.collection('Users').deleteMany({});
-  });
-
   test('It should add a selection', async () => {
     const response = await request(app)
       .post('/api/addSelection')
       .set('Authorization', `Bearer ${authToken}`)
-      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' })
-      .expect(200);
-
+      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
+    expect(response.status).toBe(200);
     expect(response.body.message).toBe('Selection added');
   });
 
   test('It should retrieve selections', async () => {
-    await db.collection('Users').updateOne(
-      { userId: authToken },
-      { $push: { selections: { title: 'Test Title', url: 'http://test.com', pageId: new ObjectId(), timestamp: new Date() } } }
-    );
+    // Add a selection first
+    await request(app)
+      .post('/api/addSelection')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
 
     const response = await request(app)
       .get(`/api/selections/${authToken}`)
-      .set('Authorization', `Bearer ${authToken}`)
-      .expect(200);
-
+      .set('Authorization', `Bearer ${authToken}`);
+    expect(response.status).toBe(200);
     expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining({ title: 'Test Title', url: 'http://test.com' })]));
   });
 
   test('It should clear selections', async () => {
-    await db.collection('Users').updateOne(
-      { userId: authToken },
-      { $set: { selections: [{ title: 'Test Title', url: 'http://test.com', pageId: new ObjectId(), timestamp: new Date() }] } }
-    );
+    // Add a selection first
+    await request(app)
+      .post('/api/addSelection')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
 
     const response = await request(app)
       .post('/api/clearSelections')
       .set('Authorization', `Bearer ${authToken}`)
-      .send({ userId: authToken })
-      .expect(200);
-
+      .send({ userId: authToken });
+    expect(response.status).toBe(200);
     expect(response.body.message).toBe('Selections cleared');
   });
 
   test('It should fail to add a selection without authentication', async () => {
     const response = await request(app)
       .post('/api/addSelection')
-      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' })
-      .expect(401);
-
+      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
+    expect(response.status).toBe(401);
     expect(response.body.message).toBe('Authentication failed');
   });
 });
-
 
