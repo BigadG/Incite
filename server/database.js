7e9c3720e730448921f185d04baa91a10136dd7d
Seif Gaber <bigadgaber@icloud.com>
2023-12-19 08:35:35 -0600

    Revert database to previous working version

--- server/database.js
+++ server/database.js
@@ -3,47 +3,25 @@ const { MongoClient } = require('mongodb');
 
 const password = process.env.MONGO_ATLAS_PASSWORD;
 const dbName = process.env.DB_NAME;
-// Ensure the username is URI encoded if needed
-const username = encodeURIComponent('bigadgaber');
-const url = `mongodb+srv://${username}:${password}@cluster0.bz2has9.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://bigadgaber:${password}@cluster0.bz2has9.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-// Create a new MongoClient instance and configure it for connection pooling
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(url);
 
 async function connect() {
-  if (!client.isConnected()) {
-    // Only connect if not already connected
-    try {
-      await client.connect();
-      console.log('Connected to MongoDB Atlas');
-    } catch (e) {
-      console.error('Failed to connect to MongoDB Atlas', e);
-      throw e; // Re-throw the error to handle it in the calling function
-    }
-  }
-  
-  const db = client.db(dbName);
-
   try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+    const db = client.db(dbName);
+
     // Ensure the `Users` collection has an index on the `uuid` field
     await db.collection('Users').createIndex({ "uuid": 1 }, { unique: true });
+
+    return db;
   } catch (e) {
-    console.error('Failed to ensure index on `Users` collection', e);
-    throw e; // Re-throw the error to handle it in the calling function
+    console.error('Failed to connect to MongoDB Atlas', e);
   }
-
-  return db;
 }
 
-// Export a cleanup function to close the client connection when the application is terminating
-function close() {
-  return client.close();
-}
-
-module.exports = { connect, close };
-
+module.exports = { connect };
 
 
