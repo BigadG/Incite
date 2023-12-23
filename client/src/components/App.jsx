c20ec83f11fcb9dde03a5580bb2b96f9c30b89c7
Seif Gaber <bigadgaber@icloud.com>
2023-12-23 14:04:57 -0600

    conditionally render NewWebPage based on application's state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import NewWebPage from './components/NewWebPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  // You can use React Router or state management to handle component rendering
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <NewWebPage />
+    </div>
+  );
+};
 
-export default App
+export default App;
+
