f97cf4909e8e63863994fa9009a767a7f6e216f2
Seif Gaber <bigadgaber@icloud.com>
2023-12-25 19:10:47 -0600

    Recreate react client from scratch

--- client/src/tests/SampleComponent.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import SampleComponent from '../components/SampleComponent';
-
-test('renders SampleComponent', () => {
-  render(<SampleComponent />);
-  expect(screen.getByText('Hello, World!')).toBeInTheDocument();
-});
