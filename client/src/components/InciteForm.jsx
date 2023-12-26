c553072a14a5cb28d271bbfc79eb277c7589cca8
Seif Gaber <bigadgaber@icloud.com>
2023-12-26 14:28:48 -0600

    Make InciteForm display results of genarateResults() func
    The generateResult function currently just creates a string based on the inputs
    This logic will be replaced later

--- client/src/components/InciteForm.jsx
+++ client/src/components/InciteForm.jsx
@@ -1,36 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/inciteStyles.css';
 
-class InciteForm extends React.Component {
-  // Handle form submission later
-  handleSubmit = (event) => {
+function InciteForm() {
+  // State to store the form inputs and result
+  const [inputs, setInputs] = useState({
+    premises: '',
+    data: '',
+    sources: '',
+  });
+  const [result, setResult] = useState('');
+
+  // Function to handle form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs(prevInputs => ({
+      ...prevInputs,
+      [name]: value,
+    }));
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // Implement logic to generate results here
+    // Call generateResult and update the result state
+    const generatedResult = generateResult();
+    setResult(generatedResult);
   };
 
-  // When I have a function to generate results, define it here
-  generateResult = () => {
-
+  // Mock function to generate a result based on the inputs
+  // You should replace this with your actual logic
+  const generateResult = () => {
+    return `Generated result based on: 
+      Premises: ${inputs.premises}, 
+      Data: ${inputs.data}, 
+      Sources: ${inputs.sources}`;
   };
 
-  render() {
-    return (
-      <main>
-        <h1>INCITE™</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" className="textbox" name="input1-Name" id="input1-Id" placeholder="Premises" />
-          <input type="text" className="textbox" name="input2-Name" id="input2-Id" placeholder="Data" />
-          <input type="text" className="textbox" name="input3-Name" id="input3-Id" placeholder="Sources" />
-          <textarea className="textbox" name="message" id="result" placeholder="Result"></textarea>
-          <br />
-          <button type="submit" className="submit">Sum It!</button>
-        </form>
-        <div id="chat-log">
-          {/* Chat log content would go here */}
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>INCITE™</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          className="textbox" 
+          name="premises" 
+          id="input1-Id" 
+          placeholder="Premises" 
+          value={inputs.premises}
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          className="textbox" 
+          name="data" 
+          id="input2-Id" 
+          placeholder="Data" 
+          value={inputs.data}
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          className="textbox" 
+          name="sources" 
+          id="input3-Id" 
+          placeholder="Sources" 
+          value={inputs.sources}
+          onChange={handleChange}
+        />
+        <textarea 
+          name="result" 
+          className="textbox" 
+          id="result" 
+          placeholder="Result"
+          value={result}
+          readOnly
+        />
+        <br />
+        <button type="submit" className="submit">Sum It!</button>
+      </form>
+      <div id="chat-log">
+        {/* Chat log content would go here */}
+      </div>
+    </main>
+  );
 }
 
 export default InciteForm;
+
