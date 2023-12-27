b14d1337ee1aa3a83d01c469fc03ed9f72db571f
Seif Gaber <bigadgaber@icloud.com>
2023-12-27 11:19:51 -0600

    Correct authMiddleware so it's not applied globally to '/api' routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const { router, register } = require('./routes');
 const authMiddleware = require('./authMiddleware');
 
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 
 const corsOptions = {
   origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
@@ -18,11 +18,8 @@ app.use(express.json());
 // Register route that should not be protected by the authMiddleware
 app.post('/api/register', register);
 
-// Apply authMiddleware to all routes except for /register
-//app.use('/api', authMiddleware);
-
 // Other routes
-app.use('/api', router);
+app.use('/api', router); // No authMiddleware here
 
 app.get('/', (req, res) => {
   res.send('Incite Server is running!');
@@ -32,7 +29,8 @@ if (require.main === module) {
   app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
   });
-}
+};
 
 module.exports = app;
+
  
