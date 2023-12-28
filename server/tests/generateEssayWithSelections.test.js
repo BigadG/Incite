7c2dee152934e30d7210340d5cc9d533ff00583a
Seif Gaber <bigadgaber@icloud.com>
2023-12-28 15:23:05 -0600

    Correct test file organization

--- server/tests/generateEssayWithSelections.test.js
+++ server/tests/generateEssayWithSelections.test.js
@@ -1,44 +1,44 @@
 const request = require('supertest');
 const app = require('../server');
 const { generateEssayContent } = require('../openaiService');
+const { closeDatabase } = require('../database'); // Ensure this function is exported from your database.js
 const authMiddleware = require('../authMiddleware');
-const { closeDatabase } = require('../database'); // You might need to implement this function
 
-jest.mock('../openaiService'); // Mock the openaiService
-jest.mock('../authMiddleware'); // Mock the authMiddleware
-
-// Mock getContentFromURLs function
-const getContentFromURLs = jest.fn(async (urls) => {
-  return "Mocked content from URLs";
-});
-
-// This is where we mock the actual implementation
-generateEssayContent.mockImplementation(async (prompts, contentFromPages) => {
-  return "Generated essay with the provided content";
-});
+jest.mock('../openaiService');
+jest.mock('../authMiddleware');
+jest.mock('../database');
 
 describe('generateEssayWithSelections endpoint', () => {
-    let server;
-  
-    // Start your server before the tests run
-    beforeAll(() => {
-      server = app.listen(3001); // Or another port that you're using for testing
+  let server;
+
+  beforeAll((done) => {
+    server = app.listen(3001, done); // Use a different port if 3001 is already in use
+  });
+
+  afterAll(async () => {
+    await server.close(); // Close the server to clean up any open handles
+    await closeDatabase(); // Ensure the database connection is closed
+  });
+
+  it('should generate essay with content from saved pages', async () => {
+    const premises = "User entered premises."; // Mock premises
+    const urls = ["http://example.com/page1", "http://example.com/page2"]; // Mock URLs
+
+    // Mock implementation of getContentFromURLs
+    const getContentFromURLs = jest.fn(async (urls) => {
+      return "Mocked content from URLs";
     });
-  
-    // Close your server after the tests run
-    afterAll(async () => {
-      await server.close(); // This should close the open handle
-      await closeDatabase(); // Make sure you close the database connection
+
+    // Mock implementation of authMiddleware to bypass actual authentication
+    authMiddleware.mockImplementation((req, res, next) => {
+      req.userId = 'mock-uuid';
+      next();
     });
-  
-    it('should generate essay with content from saved pages', async () => {
-      const premises = "User entered premises."; // Define premises here
-      const urls = ["http://example.com/page1", "http://example.com/page2"];
-      // Mock implementation of authMiddleware to bypass actual authentication
-      authMiddleware.mockImplementation((req, res, next) => {
-        req.userId = 'mock-uuid';
-        next();
-      });
+
+    // Mock implementation of generateEssayContent
+    generateEssayContent.mockImplementation(async (prompts, contentFromPages) => {
+      return "Generated essay with the provided content";
+    });
 
     const response = await request(app)
       .post('/api/generateEssayWithSelections')
@@ -51,8 +51,3 @@ describe('generateEssayWithSelections endpoint', () => {
     expect(generateEssayContent).toHaveBeenCalledWith(premises, "Mocked content from URLs");
   });
 });
-
-afterAll(async () => {
-    await closeDatabase(); // Ensure you close any open connections
-  });
-  
