8ae591268c7f15a9e77940fa8340aa876f7094a7
Seif Gaber <bigadgaber@icloud.com>
2023-12-28 15:20:56 -0600

    Define 'premises' variable
    Ensure server is closed after tests are run

--- server/tests/generateEssayWithSelections.test.js
+++ server/tests/generateEssayWithSelections.test.js
@@ -18,7 +18,22 @@ generateEssayContent.mockImplementation(async (prompts, contentFromPages) => {
 });
 
 describe('generateEssayWithSelections endpoint', () => {
+    let server;
+  
+    // Start your server before the tests run
+    beforeAll(() => {
+      server = app.listen(3001); // Or another port that you're using for testing
+    });
+  
+    // Close your server after the tests run
+    afterAll(async () => {
+      await server.close(); // This should close the open handle
+      await closeDatabase(); // Make sure you close the database connection
+    });
+  
     it('should generate essay with content from saved pages', async () => {
+      const premises = "User entered premises."; // Define premises here
+      const urls = ["http://example.com/page1", "http://example.com/page2"];
       // Mock implementation of authMiddleware to bypass actual authentication
       authMiddleware.mockImplementation((req, res, next) => {
         req.userId = 'mock-uuid';
