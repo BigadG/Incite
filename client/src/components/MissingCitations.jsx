26d1f9bbb4b86bbf73ca0e37825fd0afa94fbfd2
Seif Gaber <bigadgaber@icloud.com>
2024-01-28 12:31:41 -0700

    Only show inputs for missing citation fields and add labels

--- client/src/components/MissingCitations.jsx
+++ client/src/components/MissingCitations.jsx
@@ -6,24 +6,31 @@ function MissingCitations({ missing, onCitationChange }) {
             <h3>Missing Citation Information</h3>
             {missing.map((citation, index) => (
                 <div key={`citation-${index}`}>
-                <label>{`Citation for ${citation.url}`}</label>
-                <input
-                    type="text"
-                    onChange={(e) => onCitationChange(index, 'author', e.target.value)}
-                    placeholder="Enter author's name"
-                    value={citation.author}
-                />
-                <input
-                    type="date"
-                    onChange={(e) => onCitationChange(index, 'publicationDate', e.target.value)}
-                    placeholder="Enter publication date"
-                    value={citation.publicationDate}
-                />
+                    <label>{`Citation for ${citation.url}`}</label>
+                    {citation.missingFields.author && (
+                        <input
+                            type="text"
+                            onChange={(e) => onCitationChange(index, 'author', e.target.value)}
+                            placeholder="Enter author's name"
+                            value={citation.author || ''}
+                        />
+                    )}
+                    {citation.missingFields.publicationDate && (
+                        <div>
+                            <small>Publication Date</small>
+                            <input
+                                type="date"
+                                onChange={(e) => onCitationChange(index, 'publicationDate', e.target.value)}
+                                value={citation.publicationDate || ''}
+                            />
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
     );
 }
+
 
 export default MissingCitations;
   
