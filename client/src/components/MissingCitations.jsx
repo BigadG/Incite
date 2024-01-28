1f661f594180a43e6f47df6eca923edc326b6a21
Seif Gaber <bigadgaber@icloud.com>
2024-01-28 10:42:10 -0700

    Start styling MissingCitations.jsx
    Import styling into component

--- client/src/components/MissingCitations.jsx
+++ client/src/components/MissingCitations.jsx
@@ -1,24 +1,26 @@
+import '../styles/missingCitations.css';
+
 function MissingCitations({ missing, onCitationChange }) {
     return (
         <div className="missing-citations">
-        <h3>Missing Citation Information</h3>
-        {missing.map((citation, index) => (
-            <div key={`citation-${index}`}>
-            <label>{`Citation for URL: ${citation.url}`}</label>
-            <input
-                type="text"
-                onChange={(e) => onCitationChange(index, 'author', e.target.value)}
-                placeholder="Enter author's name"
-                value={citation.author}
-            />
-            <input
-                type="date"
-                onChange={(e) => onCitationChange(index, 'publicationDate', e.target.value)}
-                placeholder="Enter publication date"
-                value={citation.publicationDate}
-            />
-            </div>
-        ))}
+            <h3>Missing Citation Information</h3>
+            {missing.map((citation, index) => (
+                <div key={`citation-${index}`}>
+                <label>{`Citation for URL: ${citation.url}`}</label>
+                <input
+                    type="text"
+                    onChange={(e) => onCitationChange(index, 'author', e.target.value)}
+                    placeholder="Enter author's name"
+                    value={citation.author}
+                />
+                <input
+                    type="date"
+                    onChange={(e) => onCitationChange(index, 'publicationDate', e.target.value)}
+                    placeholder="Enter publication date"
+                    value={citation.publicationDate}
+                />
+                </div>
+            ))}
         </div>
     );
 }
