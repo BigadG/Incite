6c5ddd85cf8d908bb58ebffb43dcfc41d15d4800
Seif Gaber <bigadgaber@icloud.com>
2024-02-04 16:07:34 -0700

    Use final passing version of userSelections.test from main-v4 branch

--- server/tests/userSelections.test.js
+++ server/tests/userSelections.test.js
@@ -29,26 +29,11 @@ jest.mock('../database', () => ({
 }));
 
 describe('User Selections', () => {
-  test('It should add a selection', async () => {
-    const response = await request(app)
-      .post('/api/addSelection')
-      .set('Authorization', `Bearer ${authToken}`)
-      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Selection added');
-  });
+  let server;
 
-  test('It should retrieve selections', async () => {
-    await request(app)
-      .post('/api/addSelection')
-      .set('Authorization', `Bearer ${authToken}`)
-      .send({ userId: authToken, title: 'Test Title', url: 'http://test.com' });
-
-    const response = await request(app)
-      .get('/api/selections')
-      .set('Authorization', `Bearer ${authToken}`);
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining({ title: 'Test Title', url: 'http://test.com' })]));
+  beforeAll(() => {
+    // Listen on a random port
+    server = testApp.listen();
   });
 
   afterAll((done) => {
