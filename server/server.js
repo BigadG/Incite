339f331a88b9628d3ee651301232bf69cbbe538d
Bigad Gaber <bigadgaber@icloud.com>
2024-03-04 14:25:37 -0600

    Add '/api' to the end of VITE_API_BASE_URL

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { router, register } = require('./routes');
-const authMiddleware = require('./authMiddleware');
+const authMiddleware = require('./authMiddleware'); 
 
 const app = express();
 
