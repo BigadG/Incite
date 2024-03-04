2c84931b6fbda05a9128e4f23526d1977f52c1ac
Bigad Gaber <bigadgaber@icloud.com>
2024-03-04 12:11:06 -0600

    Allow all origins for CORS policy in dev mode

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const authMiddleware = require('./authMiddleware');
 
 const app = express();
 
-// Define allowed origins for CORS
+// Define allowed origins for CORS more dynamically to include all in development
 const allowedOrigins = [
   'https://incite-client-77f7b261a1a7.herokuapp.com',
   'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco'
@@ -15,37 +15,27 @@ const allowedOrigins = [
 // Enhanced CORS middleware setup to explicitly allow certain headers
 app.use(cors({
   origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
-    
-    if (allowedOrigins.includes(origin)) {
+    // Allow all origins in development or if origin is in the allowed list
+    if (process.env.NODE_ENV === 'development' || allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
+      callback(new Error('CORS policy does not allow access from the specified Origin'), false);
     }
   },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Ensure all relevant methods are allowed
-  allowedHeaders: 'Content-Type,Authorization', // Explicitly allow Authorization and Content-Type headers
-  optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+  optionsSuccessStatus: 204
 }));
-
-// Pre-flight requests handler for all routes (to support CORS)
-app.options('*', cors());
 
 app.use(express.json());
 
-// Routes
 app.post('/api/register', register);
 app.use('/api', authMiddleware, router);
 
-app.get('/', (req, res) => {
-  res.send('Incite Server is running!');
-});
+app.get('/', (req, res) => res.send('Incite Server is running!'));
 
-if (process.env.NODE_ENV !== 'test') {
-  const port = process.env.PORT || 3001;
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-}
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
 module.exports = app;
