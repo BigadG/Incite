15073d9726e30e3306510bdb76efbba7ff2688c7
Bigad Gaber <bigadgaber@icloud.com>
2024-03-04 13:53:47 -0600

    Add conditional check to ensure server only listens on a port if server.js is run directly

--- server/server.js
+++ server/server.js
@@ -6,16 +6,13 @@ const authMiddleware = require('./authMiddleware');
 
 const app = express();
 
-// Define allowed origins for CORS more dynamically to include all in development
 const allowedOrigins = [
   'https://incite-client-77f7b261a1a7.herokuapp.com',
-  'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco'
+  'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco',
 ];
 
-// Enhanced CORS middleware setup to explicitly allow certain headers
 app.use(cors({
   origin: function(origin, callback) {
-    // Allow all origins in development or if origin is in the allowed list
     if (process.env.NODE_ENV === 'development' || allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
@@ -25,7 +22,7 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 204,
 }));
 
 app.use(express.json());
@@ -35,7 +32,10 @@ app.use('/api', authMiddleware, router);
 
 app.get('/', (req, res) => res.send('Incite Server is running!'));
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+// Conditionally start the server only if the file is run directly
+if (require.main === module) {
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+}
 
 module.exports = app;
