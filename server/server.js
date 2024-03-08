7d61ea8ee79fe53e12269fe1f0fcb452a996d2c5
Bigad Gaber <bigadgaber@icloud.com>
2024-03-08 14:29:57 -0600

    Delete unused declarations

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const { router } = require('./routes');
-const authMiddleware = require('./authMiddleware');
-const process = require('process');
-const path = require('path');
 
 const app = express();
 
