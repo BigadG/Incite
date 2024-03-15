8061430ae9b3b3e0865789a51467feae4122e87a
Bigad Gaber <bigadgaber@icloud.com>
2024-03-15 07:09:53 -0500

    Revert to local working version of server.js
    - Change only the origin URL and nothing else

--- server/server.js
+++ server/server.js
@@ -3,46 +3,27 @@ const express = require('express');
 const cors = require('cors');
 const { router, register } = require('./routes');
 const authMiddleware = require('./authMiddleware');
-const path = require('path');
 
 const app = express();
 
-// Define allowed origins for CORS
-const allowedOrigins = [
-  process.env.CLIENT_ORIGIN || 'https://incite-client-77f7b261a1a7.herokuapp.com',
-  process.env.CHROME_ORIGIN || 'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco/'
-];
-
-// Set up CORS to accept requests from deployed client application and Chrome extension
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl requests, or the server itself)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Origin not allowed by CORS policy'));
-    }
-  },
-  optionsSuccessStatus: 200,
+    origin: 'https://incite-client-77f7b261a1a7.herokuapp.com',
+    optionsSuccessStatus: 200,
 }));
-
 app.use(express.json());
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 app.post('/api/register', register);
 app.use('/api', authMiddleware, router);
 
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+app.get('/', (req, res) => {
+    res.send('Incite Server is running!');
 });
 
-// Configure the server to listen on the port provided by Heroku environment
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    const port = process.env.PORT || 3001;
+    app.listen(port, () => {
+        console.log(`Server listening at http://localhost:${port}`);
+    });
+}
 
 module.exports = app;
