87013d742d470a523eeaa2fc8a44976c1688cb49
Bigad Gaber <bigadgaber@icloud.com>
2024-03-16 08:53:40 -0500

    Get rid of trailing '/' for extension origin in server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Define allowed origins for CORS
 const allowedOrigins = [
   process.env.CLIENT_ORIGIN || 'https://incite-client-77f7b261a1a7.herokuapp.com',
-  process.env.CHROME_ORIGIN || 'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco/'
+  process.env.CHROME_ORIGIN || 'chrome-extension://pljamknofgphbebllbhccjfbmdjmdfco'
 ];
 
 // Set Referrer-Policy header for all responses
