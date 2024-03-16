016a32813e40cbecbd802a9a3ddf303545360246
Bigad Gaber <bigadgaber@icloud.com>
2024-03-16 09:10:55 -0500

    Change heroku VITE_API_BASE_URL env var

--- client/src/envConfig.js
+++ client/src/envConfig.js
@@ -5,7 +5,7 @@ const getAPIBaseURL = () => {
   }
   // When running in Vite, 'import.meta.env' is available.
   return import.meta.env.VITE_API_BASE_URL || 'https://incite-d3f19169e5b5.herokuapp.com';
-};
+}; 
 
 export const API_BASE_URL = getAPIBaseURL(); 
   
