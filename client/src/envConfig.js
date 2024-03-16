250f6827910be001e7be39d355f73ffa8fbfb641
Bigad Gaber <bigadgaber@icloud.com>
2024-03-16 09:06:01 -0500

    Get rid of potentially needless '/api' in URL
    - This is causing two '/api' sections in URL, leading to wrong route

--- client/src/envConfig.js
+++ client/src/envConfig.js
@@ -4,7 +4,7 @@ const getAPIBaseURL = () => {
     return process.env.VITE_API_BASE_URL || 'http://localhost:3001';
   }
   // When running in Vite, 'import.meta.env' is available.
-  return import.meta.env.VITE_API_BASE_URL || 'https://incite-d3f19169e5b5.herokuapp.com/api';
+  return import.meta.env.VITE_API_BASE_URL || 'https://incite-d3f19169e5b5.herokuapp.com';
 };
 
 export const API_BASE_URL = getAPIBaseURL(); 
